49dd0b0d8e51e943cc0916cdeecb29eefc0d4374
Joshua Thayer <jthayer@mit.edu>
2020-02-24 20:25:18 -0800

    scanning numbers

--- src/scanner.rs
+++ src/scanner.rs
@@ -61,49 +61,74 @@ pub fn scan_token(scanner: &mut Scanner, source: &str) -> Token {
     let c = advance(scanner, source);
 
     match c {
-        "(" => make_token(TokenType::LEFTPAREN, scanner),
-        ")" => make_token(TokenType::RIGHTPAREN, scanner),
-        "{" => make_token(TokenType::LEFTBRACE, scanner),
-        "}" => make_token(TokenType::RIGHTBRACE, scanner),
-        ";" => make_token(TokenType::SEMICOLON, scanner),
-        "," => make_token(TokenType::COMMA, scanner),
-        "." => make_token(TokenType::DOT, scanner),
-        "-" => make_token(TokenType::MINUS, scanner),
-        "+" => make_token(TokenType::PLUS, scanner),
-        "*" => make_token(TokenType::STAR, scanner),
-        "/" => make_token(TokenType::SLASH, scanner),
-        "!" => {
+        '(' => make_token(TokenType::LEFTPAREN, scanner),
+        ')' => make_token(TokenType::RIGHTPAREN, scanner),
+        '{' => make_token(TokenType::LEFTBRACE, scanner),
+        '}' => make_token(TokenType::RIGHTBRACE, scanner),
+        ';' => make_token(TokenType::SEMICOLON, scanner),
+        ',' => make_token(TokenType::COMMA, scanner),
+        '.' => make_token(TokenType::DOT, scanner),
+        '-' => make_token(TokenType::MINUS, scanner),
+        '+' => make_token(TokenType::PLUS, scanner),
+        '*' => make_token(TokenType::STAR, scanner),
+        '/' => make_token(TokenType::SLASH, scanner),
+        '!' => {
             if char_match('=', scanner, source) {
                 make_token(TokenType::BANGEQUAL, scanner)
             } else {
                 make_token(TokenType::BANG, scanner)
             }
         },
-        "=" => {
+        '=' => {
             if char_match('=', scanner, source) {
                 make_token(TokenType::EQUALEQUAL, scanner)
             } else {
                 make_token(TokenType::EQUAL, scanner)
             }
         },
-        ">" => {
+        '>' => {
             if char_match('=', scanner, source) {
                 make_token(TokenType::GREATEREQUAL, scanner)
             } else {
                 make_token(TokenType::GREATER, scanner)
             }
         },
-        "<" => {
+        '<' => {
             if char_match('=', scanner, source) {
                 make_token(TokenType::LESSEQUAL, scanner)
             } else {
                 make_token(TokenType::LESS, scanner)
             }
         },
-        "\"" => string(scanner, source),
+        '"' => string(scanner, source),
+        '0'..='9' => number(scanner, source),
         _   => error_token("Unexpected character.".to_string(), scanner)
     }
 
+}
+
+fn number(scanner: &mut Scanner, source: &str) -> Token {
+    loop {
+        if is_at_end(scanner, source) {
+            break;
+        }
+        let c = peek(scanner, source);
+
+        match c {
+            '0'..='9' => (), // continue if it's a digit...
+            '.' => match peek_next(scanner, source) {
+                '0'..='9' => {
+                    advance(scanner, source); // continue if it's a digit after a '.'
+                },
+                _ => break                             // otherwise we're done
+            }
+            _ => break // break if it's anything else
+        }
+
+        advance(scanner, source);
+    }
+
+    make_token(TokenType::NUMBER, scanner)
 }
 
 fn string<'a>(scanner: &mut Scanner, source: &'a str) -> Token {
@@ -112,7 +137,7 @@ fn string<'a>(scanner: &mut Scanner, source: &'a str) -> Token {
             break;
         }
 
-        let c = peek(scanner, source).chars().next().unwrap();
+        let c = peek(scanner, source);
 
         match c {
             '"' => { advance(scanner, source); break; },
@@ -132,7 +157,7 @@ fn skip_whitespace<'a>(scanner: &mut Scanner, source: &'a str) {
             break;
         }
 
-        let c = peek(scanner, source).chars().next().unwrap();
+        let c = peek(scanner, source);
 
         match c {
             ' ' | '\r' | '\t' => {
@@ -149,9 +174,14 @@ fn skip_whitespace<'a>(scanner: &mut Scanner, source: &'a str) {
     };
 }
 
-fn peek<'a>(scanner: &Scanner, source: &'a str) -> &'a str {
-    &source[scanner.current..scanner.current+1]
+fn peek(scanner: &Scanner, source: &str) -> char {
+    source[scanner.current..].chars().next().unwrap()
 }
+
+fn peek_next(scanner: &Scanner, source: &str) -> char {
+    source[scanner.current+1..].chars().next().unwrap()
+}
+
 
 fn char_match<'a>(expected: char, scanner: &mut Scanner, source: &'a str) -> bool {
     if is_at_end(scanner, source) {
@@ -165,9 +195,9 @@ fn char_match<'a>(expected: char, scanner: &mut Scanner, source: &'a str) -> boo
     true
 }
 
-fn advance<'a>(scanner: &mut Scanner, source: &'a str) -> &'a str {
+fn advance<'a>(scanner: &mut Scanner, source: &'a str) -> char {
     scanner.current += 1;
-    &source[scanner.current - 1..scanner.current]
+    source[scanner.current - 1..].chars().next().unwrap()
 }
 
 fn is_at_end(scanner: &Scanner, source: &str) -> bool {
